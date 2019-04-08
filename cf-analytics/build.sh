0c5137d0f760c99c4670c984f6a5bbc1b4d18478
Pavan Krishna Nimmagadda <pavan.krishna@appdynamics.com>
2019-04-08 12:35:07 -0700

    fixed port

--- cf-analytics/build.sh
+++ cf-analytics/build.sh
@@ -15,7 +15,7 @@ echo "Copying new bin"
 cp -r ../bin analytics-agent/
 
 echo "Modifying Properties file"
-sed -i'' -e 's/ad.dw.http.port=9090/ad.dw.log.path=\${APPLICATION_HOME}\/logs/g' analytics-agent/conf/analytics-agent.properties
+sed -i'' -e 's/ad.dw.http.port=9090/ad.dw.http.port=\${VCAP_APP_PORT}/g' analytics-agent/conf/analytics-agent.properties
 sed -i'' -e 's/ad.controller.url=http:\/\/localhost:8090/ad.controller.url=\${APPD_ANALYTICS_CONTROLLER}/g' analytics-agent/conf/analytics-agent.properties
 sed -i'' -e 's/http.event.endpoint=http:\/\/localhost:9080/http.event.endpoint=\${APPD_ANALYTICS_EVENT_ENDPOINT}/g' analytics-agent/conf/analytics-agent.properties
 sed -i'' -e 's/http.event.name=customer1/http.event.name=\${APPD_ANALYTICS_EVENT_NAME}/g' analytics-agent/conf/analytics-agent.properties

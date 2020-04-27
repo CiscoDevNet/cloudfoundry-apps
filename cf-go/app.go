1b10dded5abfbb13bb178c35294e42076615095b
jeffaholmes <jeff.holmes@appdynamics.com>
2020-04-27 15:42:52 -0700

    Misc changes for node,java,go

--- cf-go/app.go
+++ cf-go/app.go
@@ -1,43 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "strconv"
-    "os"
-    "net/http"
-    "math/rand"
-    appd "appdynamics"
+	appd "appdynamics"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    bt := appd.StartBT("/", "")
-    time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)    
-    fmt.Fprintf(w, "Hello World")
-    appd.EndBT(bt)
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	bt := appd.StartBT("/", "")
+	time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+	fmt.Fprintf(w, "Hello World")
+	appd.EndBT(bt)
 }
 
 func main() {
-    cfg := appd.Config{}
+	cfg := appd.Config{}
 
-    cfg.AppName = os.Getenv("APPD_APPLICATION_NAME")
-    cfg.TierName = os.Getenv("APPD_TIER_NAME")
-    cfg.NodeName = os.Getenv("APPD_NODE_NAME")
-    port, err := strconv.ParseInt(os.Getenv("APPD_CONTROLLER_PORT"), 10, 16)
-    if err != nil {
-       port = 8080
-    }
-    cfg.Controller.Host = os.Getenv("APPD_CONTROLLER_HOST")
-    cfg.Controller.Port = uint16(port)
-    cfg.Controller.Account = os.Getenv("APPD_ACCOUNT_NAME")
-    cfg.Controller.AccessKey = os.Getenv("APPD_ACCOUNT_ACCESS_KEY")
-    cfg.InitTimeoutMs = 1000
+	cfg.AppName = os.Getenv("APPD_APPLICATION_NAME")
+	cfg.TierName = os.Getenv("APPD_TIER_NAME")
+	cfg.NodeName = os.Getenv("APPD_NODE_NAME") + ":" + os.Getenv("CF_INSTANCE_INDEX")
+	port, err := strconv.ParseInt(os.Getenv("APPD_CONTROLLER_PORT"), 10, 16)
 
-    err = appd.InitSDK(&cfg)
-    if err != nil {
-        fmt.Println(err)
-    }
+	if err != nil {
+		port = 8080
+	}
+	cfg.Controller.Host = os.Getenv("APPD_CONTROLLER_HOST")
+	cfg.Controller.Port = uint16(port)
+	cfg.Controller.Account = os.Getenv("APPD_ACCOUNT_NAME")
+	cfg.Controller.AccessKey = os.Getenv("APPD_ACCOUNT_ACCESS_KEY")
+	cfg.InitTimeoutMs = 1000
 
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":8080", nil)
+	err = appd.InitSDK(&cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
 }
